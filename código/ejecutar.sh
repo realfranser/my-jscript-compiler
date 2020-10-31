a79fafd63d89b0982b8a2564281b75c35e3d4a3a
jesusvallejo <jesus.vallejo.collados@gmail.com>
2020-10-31 13:29:07 +0100

    update autom

--- código/ejecutar.sh
+++ código/ejecutar.sh
@@ -1,13 +1,13 @@
 python3 rec.py -f tests/test.js > tests/debug.txt
+echo ---------------------------------
 echo ---------- tokens ---------------
+echo ---------------------------------
 cat output/tokens.txt
 echo ---------------------------------
-echo
 echo ---------- errors ---------------
+echo ---------------------------------
 cat output/errors.txt
 echo ---------------------------------
-echo
 echo ------------ ts -----------------
-cat output/ts.txt
 echo ---------------------------------
-echo
+cat output/ts.txt
