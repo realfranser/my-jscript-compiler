50f4380429953b19415678ec9ff73236316eddad
jesusvallejo <jesus.vallejo.collados@gmail.com>
2020-10-31 13:07:16 +0100

    automatizar

---
+++ código/ejecutar.sh
@@ -0,0 +1,11 @@
+/bin/usr/python3 rec.py -f tests/test.js
+echo ---------- tokens ---------------
+cat output/tokens.txt
+echo 
+echo
+echo ---------- errors ---------------
+cat output/errors.txt
+echo
+echo
+echo ----------   ts   ---------------
+cat output/ts.txt
