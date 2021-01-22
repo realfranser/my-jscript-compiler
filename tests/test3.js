fa58504a60c288a1da892fddae4f6a8bb8352539
realfranser <serranoarresejavier@gmail.com>
2021-01-22 19:52:57 +0100

    mal 3

--- tests/test3.js
+++ tests/test3.js
@@ -1,8 +1,10 @@
 let boolean grupo14;
 grupo14 = false;
+let boolean num1;
+num1 = 2;
 do {
     grupo14 = false;
-    if (grupo14) grupo14 = true;
+    if (num1) grupo14 = true;
 
 
 } while (grupo14 && true);
